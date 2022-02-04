697724db65792edc873dd3fce402cf0ee5f2c1fa
Teal Ewer <77938922+Teal-Ewer@users.noreply.github.com>
2022-02-03 21:04:47 -0500

    add reset functionality and click to check

--- js/app.js
+++ js/app.js
@@ -4,18 +4,30 @@
 
 /*------------------------ Cached Element References ------------------------*/
 const input = document.querySelector("#input");
-const button = document.querySelector("#submit-button");
+const addBtn = document.querySelector("#submit-button");
 const listUl = document.querySelector("#todo-list");
+const resetBtn = document.querySelector("#reset");
 
 /*----------------------------- Event Listeners -----------------------------*/
-button.addEventListener("click", function (e) {
+addBtn.addEventListener("click", addNewTodo);
+resetBtn.addEventListener("click", resetList);
+
+/*-------------------------------- Functions --------------------------------*/
+function addNewTodo() {
 	let newLi = document.createElement("li");
 	if (input.value === "") {
 		return alert("Please enter a valid To-do");
 	}
+	newLi.addEventListener("click", function () {
+		if (newLi.style.listStyle === "circle") {
+			newLi.style.listStyle = '"\\2713"';
+		} else newLi.style.listStyle = "circle";
+	});
 	newLi.innerText = input.value;
 	input.value = "";
 	listUl.appendChild(newLi);
-});
+}
 
-/*-------------------------------- Functions --------------------------------*/
+function resetList() {
+	listUl.innerHTML = "";
+}
